dc5548159735576f92254ac852e6b0c254175c04
Victorfernandezsaez <v.ferna.saez@gmail.com>
2023-03-13 19:02:15 +0100

    routes and error page

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,21 +1,23 @@
 import React from 'react'
 import cart from '../assets/cart.png'
 import logo from '../assets/momo__mad-symulator - copia.jpg'
-import { NavLink } from 'react-router-dom'
+import { Link } from 'react-router-dom'
+import { HashLink } from 'react-router-hash-link';
+
 function Navbar() {
     return (
         <div className="navbar py-8  bg-black h-28 w-full">
             <div className="navbar-start">
-                <a ><img width="112px" height="112px" src={logo} alt='logo' /></a>
+                <Link to='/' ><img width="112px" height="112px" src={logo} alt='logo' /></Link>
             </div>
             <div className=" hidden lg:flex">
                 <ul className="menu menu-horizontal px-1 font-F1 font-bold text-white text-md lg:space-x-18 uppercase  ">
-                    <li><a>Products</a></li>
-                    <li><a href='#features'>Features</a></li>
-                    <li><a href='#info'>More-Info</a></li>
-                    <li><a href='#compatibility'>Compatibility</a></li>
-                    <li><a href='#about'>About Us</a></li>
-                    <li><a>Contact</a></li>
+                    <li><Link>Products</Link></li>
+                    <li><HashLink smooth to='/#features'>Features</HashLink></li>
+                    <li><HashLink smooth to='/#info'>More-Info</HashLink></li>
+                    <li><HashLink to='/#compatibility'>Compatibility</HashLink></li>
+                    <li><Link to='/about'>About Us</Link></li>
+                    <li><Link to='/contact'>Contact</Link></li>
 
                 </ul>
             </div>
@@ -32,7 +34,11 @@ function Navbar() {
                             <span className="font-bold text-lg text-white">8 Items</span>
                             <span className=" text-mustard">Subtotal: $999</span>
                             <div className="card-actions">
-                                <button className="btn bg-yellow btn-block text-black font-bold hover:bg-mustard">View cart</button>
+                                <Link to='/cart'>
+                                    <button className="btn bg-yellow btn-block text-black font-bold hover:bg-mustard">
+                                        View cart
+                                    </button>
+                                </Link>
                             </div>
                         </div>
                     </div>
@@ -44,12 +50,12 @@ function Navbar() {
                     <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
                 </label>
                 <ul tabIndex={0} className="menu menu-compact dropdown-content mt-2 p-2 shadow text-black bg-gray-300 font-semibold rounded-box w-30 ">
-                    <li><a>Products</a></li>
-                    <li><a>Features</a></li>
-                    <li><a>More-Info</a></li>
-                    <li><a>Compatibility</a></li>
-                    <li><a>About Us</a></li>
-                    <li><a>Contact</a></li>
+                    <li><Link>Products</Link></li>
+                    <li><HashLink smooth to='/#features'>Features</HashLink></li>
+                    <li><HashLink smooth to='/#info'>More-Info</HashLink></li>
+                    <li><HashLink to='/#compatibility'>Compatibility</HashLink></li>
+                    <li><Link to='/about'>About Us</Link></li>
+                    <li><Link to='/contact'>Contact</Link></li>
                 </ul>
             </div>
         </div>)
